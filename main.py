37c2a3672978c4d56fcb247f96481411b70a9015
JStephens72 <jonathan_stephens@outlook.com>
2026-01-06 23:27:57 -0500

    Chapter 4 changes complete.

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
+import argparse
 import os
+import sys
+import pprint
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
-from prompts import system_prompt
+
 from call_function import available_functions, call_function
+from config import MAX_ITERS
+from prompts import system_prompt
 
-def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-
-    if api_key == None:
-        raise RuntimeError("missing api key")
-
-    client = genai.Client(api_key=api_key)
-    
-    my_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="user prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
-
-    my_prompt = args.user_prompt
-
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    response = client.models.generate_content(
+def get_response(client, messages, available_functions, system_prompt, verbose):
+   
+    genai_response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
         config=types.GenerateContentConfig(
@@ -34,34 +21,76 @@ def main():
             system_instruction=system_prompt),
     )
 
-    if args.verbose:
-        print(f"User prompt: {my_prompt}")
-        if response.usage_metadata == None:
-            raise RuntimeError("API request failed")
-        else:
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            print(f"\n")
+    pprint.pp(genai_response)
 
+    if not genai_response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
+    if verbose:
+        print(f"Prompt tokens: {genai_response.usage_metadata.prompt_token_count}\n")
+        print(f"Response tokens: {genai_response.usage_metadata.candidates_token_count}\n")
+    
+    candidates = genai_response.candidates
+    if candidates:
+        for candidate in candidates:
+            messages.append(candidate.content)
+
+    if not genai_response.function_calls:
+        return genai_response.text
+    
     function_results = []
+    for function_call in genai_response.function_calls:
+        function_call_result = call_function(function_call, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+            or not function_call_result.parts[0].function_response.response
+        ):
+            raise Exception("Bad function response")
 
-    if response.function_calls:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call)
-            if not function_call_result.parts:
-                raise Exception("No parts in function_call_result")
-            
-            first_part = function_call_result.parts[0]
-            if first_part.function_response is None:
-                raise Exception("Missing function_response")
-            
-            if first_part.function_response.response is None:
-                raise Exception("Missing response in function_response")
-            
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                resp = function_call_result.parts[0].function_response.response
-                print(f"-> {resp.get("result")}")
+        first_part = function_call_result.parts[0]
+        function_results.append(first_part)
+                                 
+    messages.append(types.Content(role="user", parts=function_results))
+
+    return None
+                
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    if args.verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    for _ in range(MAX_ITERS):
+        final_text = get_response(
+            client=client, 
+            messages=messages, 
+            available_functions=available_functions, 
+            system_prompt=system_prompt, 
+            verbose=args.verbose
+        )
+        
+        if final_text is not None:
+            print("Final response:\n")
+            print(final_text)
+            return
+        
+    print(f"Maximum interations ({MAX_ITERS}) reached without a final response")
+    sys.exit(1)
 
 
 
